l-koehler/bridgetest

commit 25022c3d7d79466d1337e43961c208a023ba80bc

fixed things (or broke them idk didnt test)

src/clientbound_translator.rs
@@ -17,13 +17,22 @@ use minetest_protocol::wire::types::{v3s16, MapNodesBulk, MapNode, MapBlock, Nod
 use azalea_client::PlayerInfo;
 use azalea_client::Client;
 use azalea_client::chat::ChatPacket;
+use azalea::inventory::ItemSlotData;
 
 use tokio::sync::mpsc::UnboundedReceiver;
 use azalea_client::Event;
 use azalea_protocol::packets::game::ClientboundGamePacket;
 use azalea_protocol::packets::game::clientbound_level_chunk_with_light_packet::{ClientboundLevelChunkWithLightPacket, ClientboundLevelChunkPacketData};
+use azalea_protocol::packets::game::clientbound_system_chat_packet::ClientboundSystemChatPacket;
 use std::sync::Arc;
 use azalea_core::position::ChunkPos;
+
+pub async fn send_item_if_missing(slotdata: ItemSlotData, slot_id: usize) {
+    todo!();
+    let item = slotdata.kind;
+    let count = slotdata.count;
+    println!("MC Client has INV:SLOT {} with ITEM {}", slot_id, item);
+}
 
 pub async fn send_message(conn: &mut MinetestConnection, message: ChatPacket) {
     let chat_packet = ToClientCommand::TCChatMessage(
@@ -38,9 +47,28 @@ pub async fn send_message(conn: &mut MinetestConnection, message: ChatPacket) {
     let _ = conn.send(chat_packet).await;
 }
 
+pub async fn send_sys_message(conn: &mut MinetestConnection, message: &ClientboundSystemChatPacket) {
+    match &message.content {
+        azalea_chat::FormattedText::Text(component) => {
+            let chat_packet = ToClientCommand::TCChatMessage(
+                Box::new(wire::command::TCChatMessageSpec {
+                    version: 1, // idk what this or message_type do
+                    message_type: 1, // but it works, dont touch it
+                    sender: String::from("System"),
+                    message: component.text.to_string(),
+                    timestamp: chrono::Utc::now().timestamp().try_into().unwrap_or(0),
+                })
+            );
+            let _ = conn.send(chat_packet).await;
+        },
+        _ => (),
+    }
+
+}
+
 // TODO THIS FUNCTION IS ONLY HERE TO TEST STUFF
 pub async fn addblock(conn: &mut MinetestConnection) {
-    utils::logger("addblock called", 3);
+    utils::logger("addblock called", 3); // :3
     let mut metadata_vec = Vec::new();
     for x in 0..15 {
         for y in 0..15 {
@@ -58,7 +86,7 @@ pub async fn addblock(conn: &mut MinetestConnection) {
                  generated: false,
                  lighting_complete: None,
                  nodes: MapNodesBulk {
-                     nodes: [MapNode {param0:130, param1:1, param2:1}; 4096],
+                     nodes: [MapNode {param0:0, param1:1, param2:1}; 4096],
                 },
                 node_metadata: NodeMetadataList {
                     metadata: metadata_vec,
@@ -142,8 +170,7 @@ fn store_level_chunk(packet_data: &ClientboundLevelChunkWithLightPacket, mc_clie
     
 }
 
-async fn send_all_chunks(mt_conn: &MinetestConnection, mt_client: &Client) {
+async fn send_all_chunks(mt_conn: &MinetestConnection, mc_client: &Client) {
     utils::logger("Skipping sending new chunk: NOT IMPLEMENTED", 3);
-    
 }
 

src/commands.rs
@@ -23,6 +23,7 @@ use minetest_protocol::wire::types;
 
 use azalea;
 use azalea_client::{Client, Account};
+use azalea::inventory::ItemSlot;
 
 use tokio::sync::mpsc::UnboundedReceiver;
 use alloc::boxed::Box;
@@ -30,6 +31,7 @@ use azalea_client::Event;
 use azalea_protocol::packets::game::ClientboundGamePacket;
 use config::Config;
 use std::net::SocketAddr;
+use alloc::sync::Arc;
 
 pub async fn mt_auto(command: ToServerCommand, conn: &mut MinetestConnection, mc_client: &azalea::Client) {
     match command {
@@ -51,17 +53,23 @@ pub async fn mc_auto(command: azalea_client::Event, mt_conn: &mut MinetestConnec
         Event::AddPlayer(player_data) => clientbound_translator::add_player(player_data, mt_conn, mt_server_state).await,
         Event::Chat(message) => clientbound_translator::send_message(mt_conn, message).await,
         Event::Tick => on_minecraft_tick(mt_conn, mc_client, mt_server_state).await,
-        Event::Packet(packet_value) => match *packet_value {
-            ClientboundGamePacket::ChunkBatchStart(_) => clientbound_translator::chunkbatch(mt_conn, mc_conn, &mc_client).await,
-            _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
-        },
+        Event::Packet(packet_value) => mc_gamepack_auto(packet_value, mt_conn, mc_client, mc_conn, command_name).await,
+        _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
+    }
+}
+
+pub async fn mc_gamepack_auto(command: Arc<ClientboundGamePacket>, mt_conn: &mut MinetestConnection, mc_client: &azalea::Client, mc_conn: &mut UnboundedReceiver<Event>, command_name: &str) {
+    let packet_value: &ClientboundGamePacket = &*command; // not entirely useless, derefs a Arc<>
+    match packet_value {
+        ClientboundGamePacket::ChunkBatchStart(_) => clientbound_translator::chunkbatch(mt_conn, mc_conn, &mc_client).await,
+        ClientboundGamePacket::SystemChat(message) => clientbound_translator::send_sys_message(mt_conn, &message.clone()).await,
         _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
     }
 }
 
 pub async fn on_minecraft_tick(mt_conn: &mut MinetestConnection, mc_client: &azalea::Client, mt_server_state: &mut MTServerState) {
     // stuff to do on each tick
-    utils::logger("[Minecraft] S->C Tick, regular each-tick stuff will be done", 1);
+    utils::logger("[Minecraft] S->C Tick", 1);
     // sync the inventory from mc_client over to the minetest client (if it changed)
     resync_inventory(mc_client, mt_conn).await;
 }
@@ -115,7 +123,7 @@ pub async fn handshake(command: ToServerCommand, conn: &mut MinetestConnection, 
     let _ = conn.send(auth_accept_command).await;
     utils::logger("[Minetest] S->C AuthAccept", 1);
     utils::logger("[Minecraft] Logging in...", 1);
-    
+
     // TODO: Change this line to allow online accounts
     let mc_server_addr: SocketAddr = settings.get_string("mc_server_addr").expect("Failed to read config!")
                                              .parse().expect("Failed to parse mc_server_addr!");
@@ -125,7 +133,21 @@ pub async fn handshake(command: ToServerCommand, conn: &mut MinetestConnection, 
 }
 
 pub async fn resync_inventory(mc_client: &Client, mt_conn: &mut MinetestConnection) {
-    // TODO take the inventory of mc_client and move it to mt_conn
+    // only sync when the player currently does NOT have another inventory open
+    match mc_client.menu() {
+        azalea::inventory::Menu::Player(player) => (),
+        _ => return,
+    }
+    // iterate over each item
+    let mc_inventory_range = mc_client.menu().player_slots_range();
+    let mut mc_item: ItemSlot;
+    for slot_id in mc_inventory_range {
+        mc_item = mc_client.menu().slot(slot_id).expect("Slot out-of-range!").clone();
+        match mc_item {
+            ItemSlot::Empty => (),
+            ItemSlot::Present(slotdata) => clientbound_translator::send_item_if_missing(slotdata, slot_id).await,
+        }
+    }
 }
 
 

Cargo.toml
@@ -13,6 +13,7 @@ azalea-protocol = { git = "https://github.com/azalea-rs/azalea" }
 azalea-world = { git = "https://github.com/azalea-rs/azalea" }
 azalea-core = { git = "https://github.com/azalea-rs/azalea" }
 azalea-registry = { git = "https://github.com/azalea-rs/azalea" }
+azalea-chat = { git = "https://github.com/azalea-rs/azalea" }
 base64 = "0.21.7"
 bytes = "1.5.0"
 chrono = "0.4.34"

src/mt_definitions.rs
@@ -4,8 +4,9 @@
 
 use azalea::core::particle;
 use azalea::entity::metadata::Text;
-use minetest_protocol::wire::command::{AnnounceMediaSpec, MediaSpec, ItemdefSpec, NodedefSpec, ToClientCommand};
-use minetest_protocol::wire::types::{ v3f, AlignStyle, ContentFeatures, DrawType, ItemAlias, ItemDef, ItemType, ItemdefList, MediaAnnouncement, MediaFileData, NodeBox, Option16, SColor, SimpleSoundSpec, TileAnimationParams, TileDef, BlockPos, NodeMetadata, StringVar, Inventory, NodeDefManager }; // AAAAAA
+use minetest_protocol::wire::command::{AnnounceMediaSpec, MediaSpec, ItemdefSpec, NodedefSpec, ToClientCommand, InventoryFormspecSpec};
+use minetest_protocol::wire::types::{ v3f, AlignStyle, ContentFeatures, DrawType, ItemAlias, ItemDef, ItemType, ItemdefList, MediaAnnouncement, MediaFileData, NodeBox, Option16, SColor, SimpleSoundSpec, TileAnimationParams, TileDef, BlockPos, NodeMetadata, StringVar, Inventory, NodeDefManager
+}; // AAAAAA
 
 use alloc::boxed::Box;
 use config::Config;
@@ -16,9 +17,19 @@ use std::fs;
 use std::io::{ Cursor, Write, Read, copy };
 
 use crate::utils;
+use crate::settings;
 use sha1::{Sha1, Digest};
 use base64::{Engine as _, engine::general_purpose};
 use serde_json;
+
+pub fn get_inventory_formspec() -> ToClientCommand {
+    let formspec_command = ToClientCommand::InventoryFormspec(
+        Box::new(InventoryFormspecSpec{
+            formspec: String::from(settings::INV_FORMSPEC),
+        })
+    );
+    return formspec_command;
+}
 
 pub fn get_metadata_placeholder(x_pos: u16, y_pos: u16, z_pos: u16) -> (BlockPos, NodeMetadata) {
     let blockpos = BlockPos {

src/settings.rs
@@ -21,6 +21,13 @@ arcticdata_blocks = \"https://raw.githubusercontent.com/Articdive/ArticData/1.20
 arcticdata_items = \"https://raw.githubusercontent.com/Articdive/ArticData/1.20.4/1_20_4_items.json\"
 ";
 
+pub const INV_FORMSPEC: &str = "\
+size[8,7.5]
+image[1,0.6;1,2;player.png]
+list[current_player;main;0,3.5;8,4;]
+list[current_player;craft;3,0;3,3;]
+list[current_player;craftpreview;7,1;1,1;]";
+
 /*
  * 0: Display every recieved packet
  * 1: Display some extra status messages (and every SENT packet lol i suck at this)

src/translator.rs
@@ -67,7 +67,9 @@ pub async fn client_handler(_mt_server: MinetestServer, mut mt_conn: MinetestCon
     let _ = mt_conn.send(media_packets.3).await;
     utils::logger("[Minetest] S->C Media (Item)", 1);
     let _ = mt_conn.send(media_packets.4).await;
-    
+    // utils::logger("[Minetest] S->C Inventory Formspec", 1);
+    // let _ = mt_conn.send(mt_definitions::get_inventory_formspec()).await;
+
     utils::logger("[Minetest] S->C Itemdef", 1);
     let _ = mt_conn.send(mt_definitions::get_item_def_command(&settings).await).await;
     utils::logger("[Minetest] S->C Nodedef", 1);

commit 9b1f6e3368d0193e12ae1a3c9f13e21eac84d80b

25022c3 broke stuff, not surprised

src/clientbound_translator.rs
@@ -116,21 +116,6 @@ pub async fn initialize_16node_chunk(x_pos:i16, y_pos:i16, z_pos:i16, conn: &mut
     let _ = conn.send(addblockcommand).await;
 }
 
-pub async fn setblock(x: i16, y: i16, z: i16, id: u16, mt_conn: &mut MinetestConnection) {
-    let addblockcommand = ToClientCommand::Addnode(
-        Box::new(wire::command::AddnodeSpec {
-            pos: v3s16 { x, y, z },
-            keep_metadata: true,
-            node: MapNode {
-                param0: id,
-                param1: 1,
-                param2: 1,
-            }
-        })
-    );
-    let _ = mt_conn.send(addblockcommand).await;
-}
-
 pub async fn add_player(player_data: PlayerInfo, conn: &mut MinetestConnection, mt_server_state: &mut MTServerState) {
     let new_user: String = player_data.profile.name.to_string();
     mt_server_state.players.push(new_user);
@@ -149,6 +134,7 @@ pub async fn chunkbatch(mt_conn: &mut MinetestConnection, mc_conn: &mut Unbounde
     // called by a ChunkBatchStart
     // first let azalea do everything until ChunkBatchFinished,
     // then move the azalea world over to the client
+    println!("chunkbatch");
     loop {
         tokio::select! {
             t = mc_conn.recv() => {
@@ -160,7 +146,7 @@ pub async fn chunkbatch(mt_conn: &mut MinetestConnection, mc_conn: &mut Unbounde
                             Event::Packet(packet_value) => match Arc::unwrap_or_clone(packet_value) {
                                 ClientboundGamePacket::LevelChunkWithLight(packet_data) => {
                                     utils::logger("[Minecraft] S->C LevelchunkWithLight", 1);
-                                    store_level_chunk(&packet_data, mc_client);
+                                    send_level_chunk(&packet_data, mt_conn).await;
                                 },
                                 ClientboundGamePacket::ChunkBatchFinished(_) => {
                                     utils::logger("[Minecraft] S->C ChunkBatchFinished", 1);
@@ -179,15 +165,28 @@ pub async fn chunkbatch(mt_conn: &mut MinetestConnection, mc_conn: &mut Unbounde
     }
 }
 
-fn store_level_chunk(packet_data: &ClientboundLevelChunkWithLightPacket, mc_client: &Client) {
+pub async fn send_level_chunk(packet_data: &ClientboundLevelChunkWithLightPacket, mt_conn: &mut MinetestConnection) {
+    println!("haii :3");
     // Parse packet
     let ClientboundLevelChunkWithLightPacket {x: chunk_x_pos, z: chunk_z_pos, chunk_data: chunk_packet_data, light_data: light_packet_data} = packet_data;
     let ClientboundLevelChunkPacketData { heightmaps: chunk_heightmaps, data: chunk_data, block_entities: chunk_entities } = chunk_packet_data;
     utils::logger(&format!("[Minecraft] Server sent chunk x/z {}/{}", chunk_x_pos, chunk_z_pos), 1);
-    let chunk_location: ChunkPos = ChunkPos { x: *chunk_x_pos, z: *chunk_z_pos };
-    // send chunk over
-    // TODO: this is terribly slow. it iterates the chunk, pushing each node one-at-a-time.
-    
+    //let chunk_location: ChunkPos = ChunkPos { x: *chunk_x_pos, z: *chunk_z_pos }; // unused
+    // send chunk to the MT client
+    let mut nodearr: [MapNode; 4096] = [MapNode { param0: 0, param1: 1, param2: 1 }; 4096];
+    let mut node: MapNode;
+    // for each y level (mc chunks go from top to bottom, while mt chunks are 16 nodes high)
+    for chunk_y_pos in -4..20 {
+        for array_node in nodearr.iter_mut() {
+            println!("CHUNKU8LEN {}", chunk_data.len());
+            // detailed explanation of this iterator hidden in initialize_16node_chunk
+            // but it basically assigns each node that will be sent to mt an ID as a value
+            node = MapNode { param0: 17, param1: 1, param2: 1 };
+            *array_node = node;
+        }
+        // map the i32 mc chunks to i16 mt ones, possibly overflowing them (intentionally, better than crashing ig)
+        initialize_16node_chunk(*chunk_x_pos as i16, chunk_y_pos, *chunk_z_pos as i16, mt_conn, nodearr).await;
+    }
 }
 
 async fn send_all_chunks(mt_conn: &MinetestConnection, mc_client: &Client) {

src/commands.rs
@@ -42,34 +42,29 @@ pub async fn mt_auto(command: ToServerCommand, conn: &mut MinetestConnection, mc
         ToServerCommand::ModchannelLeave(_) => utils::logger("[Minetest] Client sent ModchannelLeave, this does not exist in MC", 2),
         ToServerCommand::TSModchannelMsg(_) => utils::logger("[Minetest] Client sent TSModchannelMsg, this does not exist in MC", 2),
         ToServerCommand::TSChatMessage(specbox) => serverbound_translator::send_message(&mc_client, specbox),
-        _ => utils::logger("[Minetest] Got unimplemented command, dropping packet!", 2) // Drop packet if unable to match
+        _ => utils::logger(&format!("[Minetest] Got unimplemented command, dropping packet {:?}!", command), 2) // Drop packet if unable to match
     }
 }
 
 pub async fn mc_auto(command: azalea_client::Event, mt_conn: &mut MinetestConnection, mc_client: &azalea::Client, mt_server_state: &mut MTServerState, mc_conn: &mut UnboundedReceiver<Event>) {
     let cloned_command = command.clone();
     let command_name = utils::mc_packet_name(&cloned_command);
-    match command {
+    match command { // ik i shouldn't match strings but i fought the borrow checker about this for ONE HOUR, giving up
         Event::AddPlayer(player_data) => clientbound_translator::add_player(player_data, mt_conn, mt_server_state).await,
         Event::Chat(message) => clientbound_translator::send_message(mt_conn, message).await,
         Event::Tick => on_minecraft_tick(mt_conn, mc_client, mt_server_state).await,
-        Event::Packet(packet_value) => mc_gamepack_auto(packet_value, mt_conn, mc_client, mc_conn, command_name).await,
-        _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
-    }
-}
-
-pub async fn mc_gamepack_auto(command: Arc<ClientboundGamePacket>, mt_conn: &mut MinetestConnection, mc_client: &azalea::Client, mc_conn: &mut UnboundedReceiver<Event>, command_name: &str) {
-    let packet_value: &ClientboundGamePacket = &*command; // not entirely useless, derefs a Arc<>
-    match packet_value {
-        ClientboundGamePacket::ChunkBatchStart(_) => clientbound_translator::chunkbatch(mt_conn, mc_conn, &mc_client).await,
-        ClientboundGamePacket::SystemChat(message) => clientbound_translator::send_sys_message(mt_conn, &message.clone()).await,
+        Event::Packet(packet_value) => match (*packet_value).clone() {
+            ClientboundGamePacket::ChunkBatchStart(_) => clientbound_translator::chunkbatch(mt_conn, mc_conn, &mc_client).await,
+            ClientboundGamePacket::SystemChat(message) => clientbound_translator::send_sys_message(mt_conn, &message.clone()).await,
+            _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
+        }
         _ => utils::logger(&format!("[Minecraft] Got unimplemented command, dropping {}", command_name), 2),
     }
 }
 
 pub async fn on_minecraft_tick(mt_conn: &mut MinetestConnection, mc_client: &azalea::Client, mt_server_state: &mut MTServerState) {
     // sync the inventory from mc_client over to the minetest client (if it changed)
-    resync_inventory(mc_client, mt_conn).await;
+    //resync_inventory(mc_client, mt_conn).await;
 }
 
 pub async fn handshake(command: ToServerCommand, conn: &mut MinetestConnection, mt_server_state: &mut MTServerState, settings: &Config) -> (azalea::Client, UnboundedReceiver<azalea::Event>) {